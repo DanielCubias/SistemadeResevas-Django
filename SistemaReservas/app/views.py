dc32119c1751f932820af8df26b1d59a4ed96e21
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-12-13 11:05:02 +0100

    Archivo template creado correctamente

--- SistemaReservas/app/views.py
+++ SistemaReservas/app/views.py
@@ -1,7 +1,11 @@
+from tempfile import template
+
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola mundo")
+    template = loader.get_template('login.html')
+    return HttpResponse(template.render())
