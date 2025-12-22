e8d7f2319656d8ede2cc860b2a28980a239599e0
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-12-22 16:17:45 +0100

    Creando logica para que usuario no pueda tener mas de 1 reserva

--- SistemaReservas/app/models.py
+++ SistemaReservas/app/models.py
@@ -1,3 +1,6 @@
+# esto permite lanzar un error de validacion cuando algo no cumple las reglas del modelo
+from django.core.exceptions import ValidationError
+
 from django.db import models
 
 # Create your models here.
@@ -20,5 +23,26 @@ class Reserva(models.Model):
     hora_fin = models.TimeField()
     creado_en = models.DateTimeField(auto_now_add=True)
 
+
+def clean(self):
+    reservas = Reserva.objects.filter(
+        usuario = self.usuario,
+        fecha = self.fecha,
+    ).exclude(pk=self.pk)
+    # evita que se compare consigo misma
+
+    for x in reservas:
+        if self.hora_inicio < x.hora_fin and self.hora_fin > x.hora_inicio:
+            raise ValidationError("La reserva se solapa con otra existente")
+
+def save(self, *args , **kwargs):
+    self.full_clean()
+    # es mejor esta que "self.clean()", por que llama a clean , valida campos, valida modelos completos
+    # tambien porque save() no garantiza validaciones en el admin ni el todos los flujos
+    # en cambio full_clean() asegura coherencia a nivel modelo
+    super().save(*args, **kwargs)
+
     def __str__(self):
         return f"{self.usuario} - {self.fecha} {self.hora_inicio}-{self.hora_fin}"
+
+
