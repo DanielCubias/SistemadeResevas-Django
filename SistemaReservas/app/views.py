b314b1aea4d325247082dce2929f962fedf5a5c7
danielramirez <2013.1b.cubdaniel@gmail.com>
2025-12-30 15:42:58 +0100

    Mejorando logica de creacion de usaurio y utilizacion de messages

--- SistemaReservas/app/views.py
+++ SistemaReservas/app/views.py
@@ -4,16 +4,21 @@ from tempfile import template
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth import get_user_model
+from .models import Reserva
 
 
 # utilizo lo siguinete porque tengo un modelo de usuario personalizado
 # si no, dara error : Manager isn't available; 'auth.User' has been swapped for 'app.Usuario'
 # para arreglarlo he importado mi modelo personalizado
 
-from django.contrib.auth import get_user_model
+
+
+
 User = get_user_model()
 
-from .models import Reserva
+
 
 
 # Create your views here.
@@ -32,16 +37,27 @@ def registro(request):
         password = request.POST["password"]
         password2 = request.POST["password2"]
 
+        if not username or not email or not password or not password2:
+            messages.error(request, "hacen faltan campos por rellenar")
+            return  render(request, "registro.html")
+
         if password != password2:
-            return render(request, "registro.html", {"error": "Las contraseñas no coinciden"})
+            messages.error(request, "las contraseñas no coinciden")
+            return render(request,"registro.html")
 
         User = get_user_model()
+
         if User.objects.filter(username=username).exists():
-            return render(request, "registro.html", {"error": "El nombre de usuario ya existe"})
+            messages.error(request, "El nombre de usuario ya existe")
+            return render(request, "registro.html")
 
+        User.objects.create_user(
+            username=username,
+            email = email,
+            password= password
+        )
 
-        User.objects.create_user(username=username, email=email, password=password)
-
+        messages.success(request,"Usuari creado correctamente")
         return redirect("login")
 
     return render(request, "registro.html")
